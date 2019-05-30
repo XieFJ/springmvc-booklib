ccd470bfc609ae95924b63c13eccec835dd7bfb7
xfj <fengjun_xie@163.com>
2019-05-30 22:06:18 +0800

    springmvc与freemarker整合

--- src/cn/edu/zucc/booklib/controller/TestController.java
+++ src/cn/edu/zucc/booklib/controller/TestController.java
@@ -1,13 +1,17 @@
 package cn.edu.zucc.booklib.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
     @RequestMapping(value="/test")
-    public String test(){
-//    实际返回的是WEB-INF/html/test.html ,springmvc-config.xml中配置过前后缀
-        return "/WEB-INF/html/test.html";
-    }
+    public String handleRequest(HttpServletRequest request) throws Exception {
+    	request.setAttribute("msg", "hello world");
+    	
+		return "test";
+	}
+    
 }
