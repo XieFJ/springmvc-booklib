2861099b277f05e37b2d40a3f406a6ade9356b85
xfj <fengjun_xie@163.com>
2019-06-01 16:02:02 +0800

    删除UserDAO

--- src/cn/edu/zucc/booklib/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.zucc.booklib.dao;
-
-import java.util.List;
-
-import cn.edu.zucc.booklib.databean.BeanSystemUser;
-
-public interface UserDAO {
-	public List<BeanSystemUser> findAll();
-}
--- src/cn/edu/zucc/booklib/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.zucc.booklib.service;
-
-import java.util.List;
-
-import cn.edu.zucc.booklib.databean.BeanSystemUser;
-
-public interface UserService {
-	public List<BeanSystemUser> findAll();
-}
